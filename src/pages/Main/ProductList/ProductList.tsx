99a0cc5b1cebae61d87241304a98c7d6acc888e7
javifm86 <javifm86@gmail.com>
2021-12-21 16:33:25 +0100

    Product list component

--- src/pages/Main/ProductList/ProductList.tsx
+++ src/pages/Main/ProductList/ProductList.tsx
@@ -1,36 +1,58 @@
-import React, { FunctionComponent } from 'react';
-interface ProductListProps {}
+import { FunctionComponent } from 'react';
+import ProductCard from '../../../components/ProductCard/ProductCard';
+import { Product } from '../../../models/product.model';
+interface ProductListProps {
+  products: Product[];
+}
 
-const ProductList: FunctionComponent<ProductListProps> = () => {
+const ProductList: FunctionComponent<ProductListProps> = ({ products }) => {
+  console.warn(products);
+
+  const added = (val: boolean) => {
+    console.log('added callback:', val);
+  };
+  const addedFav = (val: boolean) => {
+    console.log('addedFav callback:', val);
+  };
+
   return (
-    <div className="flex flex-wrap -mx-2 mb-8">
-      {/* <ng-container *ngFor="let item of products"> */}
-      <article className="w-full md:w-1/2 xl:w-1/3 px-4 mb-8">
-        Hi
-        {/* <app-product-card
-        [img]="item.image_url"
-        [stock]="item.stock"
-        [name]="item.productName"
-        [price]="item.price"
-        [description]="item.productDescription"
-        [inBasket]="item.inBasket"
-        [favorite]="item.favorite === 1 || item.favorite === '1'"
-        (added)="addPressed(item);"
-        (addedToFav)="addedToFav($event, item);"
-      ></app-product-card> */}
-      </article>
-      {/* </ng-container> */}
-    </div>
+    <>
+      {products.length > 0 && (
+        <div className="flex flex-wrap -mx-2 mb-8">
+          {products.map((product) => {
+            return (
+              <article
+                key={product.id}
+                className="w-full md:w-1/2 xl:w-1/3 px-4 mb-8"
+              >
+                <ProductCard
+                  key={product.id}
+                  img={product.image_url}
+                  stock={product.stock}
+                  name={product.productName}
+                  price={product.price}
+                  description={product.productDescription}
+                  inBasket={product.inBasket != null ? product.inBasket : false}
+                  favorite={product.favorite === 1}
+                  added={added}
+                  addedToFav={addedFav}
+                ></ProductCard>
+              </article>
+            );
+          })}
+        </div>
+      )}
+      {products.length === 0 && (
+        <div
+          className="w-full bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4"
+          role="alert"
+        >
+          <p className="font-bold">No results</p>
+          <p>There are no products.</p>
+        </div>
+      )}
+    </>
   );
-
-  {
-    /* <ng-template #templateEmpty>
-  <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
-    <p className="font-bold">No results</p>
-    <p>There are no products.</p>
-  </div>
-</ng-template>; */
-  }
 };
 
 export default ProductList;
